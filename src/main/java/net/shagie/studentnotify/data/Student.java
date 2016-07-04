3426aa285ddaa7c42f8a9f3f197d4c7e23b54c35
shagie <turnmichael@gmail.com>
2016-07-04 11:35:11 -0500

    Remove 'notifications' from student object

--- src/main/java/net/shagie/studentnotify/data/Student.java
+++ src/main/java/net/shagie/studentnotify/data/Student.java
@@ -1,22 +1,13 @@
 package net.shagie.studentnotify.data;
 
 import java.util.List;
-import java.util.Map;
 
 public class Student {
     private String id;
     private List<Semester> grades;
     private String name;
-    private Map<String, List<Integer>> notifications;
 
     public Student() {
-    }
-
-    public Student(String id, List<Semester> grades, String name, Map<String, List<Integer>> notifications) {
-        this.id = id;
-        this.grades = grades;
-        this.name = name;
-        this.notifications = notifications;
     }
 
     public String getName() {
@@ -25,14 +16,6 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Map<String, List<Integer>> getNotifications() {
-        return notifications;
-    }
-
-    public void setNotifications(Map<String, List<Integer>> notifications) {
-        this.notifications = notifications;
     }
 
     public String getId() {
@@ -57,7 +40,6 @@ public class Student {
                 "id='" + id + '\'' +
                 ", grades=" + grades +
                 ", name='" + name + '\'' +
-                ", notifications=" + notifications +
                 '}';
     }
 }
