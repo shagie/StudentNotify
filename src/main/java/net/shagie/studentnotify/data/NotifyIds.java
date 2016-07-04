e35d69836ada002c619d4ecc53597747df72253b
shagie <turnmichael@gmail.com>
2016-07-04 16:41:12 -0500

    Move id constants to class

---
+++ src/main/java/net/shagie/studentnotify/data/NotifyIds.java
@@ -0,0 +1,8 @@
+package net.shagie.studentnotify.data;
+
+public final class NotifyIds {
+    public final static int ENROLL = 1;
+    public final static int STRAIGHT_AS = 2;
+
+    private NotifyIds() {}
+}
