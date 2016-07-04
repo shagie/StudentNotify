44fca22a6954e80108f8b5b7104a2052c4f125d5
shagie <turnmichael@gmail.com>
2016-07-04 17:57:56 -0500

    Notifications for last semester rules
    Create 'last semester' rules and related objects.

--- src/main/java/net/shagie/studentnotify/data/NotifyIds.java
+++ src/main/java/net/shagie/studentnotify/data/NotifyIds.java
@@ -3,6 +3,9 @@ package net.shagie.studentnotify.data;
 public final class NotifyIds {
     public final static int ENROLL = 1;
     public final static int STRAIGHT_AS = 2;
+    public final static int TRANSFER = 3;
+    public final static int SPORTS_ELIGIBILITY = 4;
+    public final static int GPA = 5;
 
     private NotifyIds() {}
 }
